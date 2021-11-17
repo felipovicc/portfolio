68fa5ee94e0d169fdf908307bf557cba6ef4a81d
felipovicc <felipovicc@gmail.com>
2021-11-17 15:28:40 +0100

    Responsive navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,45 +1,68 @@
-import React from 'react';
-import { FaArrowRight } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { MdMarkEmailUnread } from 'react-icons/md';
 import { HiSun, HiMoon } from 'react-icons/hi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const NavBar = ({ isDark, toggleDark }) => {
+  const [showMenu, setShowMenu] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowMenu(window.innerWidth >= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <header className='text-gray-300 bg-gray-700 dark:bg-gray-800 md:sticky top-0 z-10 animate__animated animate__fadeInDown shadow-lg'>
-      <div className='container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-center'>
-        <div className='title-font font-medium text-white mb-4 md:mb-0'>
-          <a href='#about' className='md:ml-3 text-xl'>
+      <div className='order-0 mx-6 py-4 flex items-center justify-between flex-wrap'>
+        <div>
+          <a
+            href='#about'
+            className='font-medium text-white md:ml-3 text-xl mb-2'
+          >
             Felipe Noriega
           </a>
         </div>
-        <nav className='mx-auto md:m-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center'>
-          <a href='#projects' className='mr-5 hover:text-white'>
-            Portfolio
-          </a>
-          <a href='#skills' className='mr-5 hover:text-white'>
-            Skills
-          </a>
-          {/* <a href='#testimonials' className='mr-5 hover:text-white'>
-                        Testimonials
-                    </a> */}
-        </nav>
-        <div className='justify-center'>
-          <a
-            href='#contact'
-            className='inline-flex items-center border border-gray-500 py-1 px-3 focus:outline-none hover:text-white hover:bg-gray-500 rounded text-base mt-4 md:mt-0 sm:mb-4 md:mb-0'
-          >
-            &nbsp; Contact Me
-            <FaArrowRight size={16} className='ml-1' />
-          </a>
-        </div>
-        <div className='mx-4 justify-center items-center mt-4 md:mt-0'>
+        {showMenu && (
+          <nav className='flex flex-1 flex-column md:flex-row items-center justify-between order-3 md:order-2 mt-2 md:mt-0'>
+            <div className='md:ml-14'>
+              <a href='#projects' className='mx-2 hover:text-white'>
+                Portfolio
+              </a>
+              <a href='#skills' className='mx-2 hover:text-white'>
+                Skills
+              </a>
+            </div>
+            <div className='mx-2'>
+              <a
+                href='#contact'
+                className='flex border border-gray-500 ml-2 py-1 px-2 focus:outline-none hover:text-white hover:bg-gray-500 rounded text-base'
+              >
+                <MdMarkEmailUnread size={24} className='mr-2' />
+                Contact
+              </a>
+            </div>
+          </nav>
+        )}
+        <div className='flex flex-2 md:order-3 order-2 justify-center items-center'>
           <button
-            className='border-gray-500 py-1 px-1.5 border focus:outline-none hover:text-white hover:bg-gray-500 rounded '
+            className='md:hidden border border-gray-500 ml-2 py-1 px-1.5 focus:outline-none hover:text-white hover:bg-gray-500 rounded text-base'
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <GiHamburgerMenu size={24} />
+          </button>
+          <button
+            className='border border-gray-500 ml-2 py-1 px-1.5 focus:outline-none hover:text-white hover:bg-gray-500 rounded text-base'
             onClick={toggleDark}
           >
             {isDark ? (
-              <HiSun className='h-6 w-6 animate__animated animate__fadeIn' />
+              <HiSun size={24} className='animate__animated animate__fadeIn' />
             ) : (
-              <HiMoon className='h-6 w-6 animate__animated animate__fadeIn' />
+              <HiMoon size={24} className='animate__animated animate__fadeIn' />
             )}
           </button>
         </div>
